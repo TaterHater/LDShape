f007a54f5ddd042efaadd35094986a9943e488aa
Nick B <nick_beeson@yahoo.com>
2016-04-16 17:12:12 -0600

    added spoopy sprites, and everything nice

---
+++ LDShapeshift/Assets/Title stuff/LoadLevel.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+
+    public float delay = 10.0f;
+
+    public void changeScene()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+
+    
+    void Start()
+    {
+        this.Invoke("changeScene", delay);
+    }
+
+   
+   
+    }
