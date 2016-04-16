2b16121e80f89b6297d5a9e1d573051a2f3c33d9
Nick B <nick_beeson@yahoo.com>
2016-04-16 16:18:55 -0600

    added movement

---
+++ LDShapeshift/Assets/scripts/playerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+    public float speed;
+
+	
+	
+	void FixedUpdate () {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            transform.Translate(Vector3.left * Input.GetAxis("Horizontal") * speed);
+        }
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            transform.Translate(Vector3.up * Input.GetAxis("Vertical") * speed*-1);
+        }
+
+    }
+}
---
+++ LDShapeshift/Assets/scripts/soundTrigger.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class soundTrigger : MonoBehaviour {
+
+    public GameObject Player;
+    public AudioClip[] myClip;
+    public int overrideChoice;
+    //bool hasPlayed=false;
+    public bool isActive = false;
+   
+    void Start()
+    {
+
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("hiiit");
+        isActive = true;
+
+        if (other.tag == "Player")
+        {
+            if (overrideChoice != 20)
+            {
+                GetComponent<AudioSource>().clip = myClip[overrideChoice];
+            }
+            Debug.Log("hit");
+            GetComponent<AudioSource>().clip = myClip[Random.Range(0, myClip.Length)];
+            GetComponent<AudioSource>().Play();
+
+        }
+
+
+    }
+}
